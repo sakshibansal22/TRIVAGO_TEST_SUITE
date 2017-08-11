47565914cb033c9423ea1e181eca0dc818c5ab17
sakshibansal22 <sakshibansal22@gmail.com>
2017-08-11 19:24:59 +0530

    Added 2 Test Cases

--- TrivagoTest/src/test/java/com/trivago/TestCases.java
+++ TrivagoTest/src/test/java/com/trivago/TestCases.java
@@ -28,9 +28,7 @@ public class TestCases extends DriverFactory {
 	public static void TC1_VerifyHeaderNameAfterSelectingDestination() {
 		
 		log.info("Verify Article Header Name After Selecting The Destination ... Starts ");
-		double i=21.2%10;
-		log.info(i);
-
+		
 		// Click on the Menu Icon
 		UtilityClass.clickOnLocator(System.getProperty(CommonConstants.MenuXpath));
 		// Select Destination "USA"
@@ -44,5 +42,67 @@ public class TestCases extends DriverFactory {
 		log.info("Verify Article Header Name After Selecting The Destination ... Ends ");
 
 	}
+	
+@Test(dependsOnMethods = { "TC1_VerifyHeaderNameAfterSelectingDestination" },description="Click on home page icon ")
+	
+	public static void TC1_ClickOnHomePageIcon() {
+		
+		log.info("Click on home page icon ... Starts ");
+		
+		
+		// Click on HomePage ICOn
+		UtilityClass.clickOnLocator(System.getProperty(CommonConstants.HomeIconXapth));
+		// Get Header of Article
+		String CountryName = UtilityClass.getTextOfLocator(System.getProperty(CommonConstants.ArticleheaderXapth));
+		System.out.println(CountryName);
+		// verify Header should be same as the destination selcted
+		Assert.assertTrue(CountryName.equals("USA"), "Article Header Name is Not Shown Proper");
+
+		log.info("Click on home page icon ... Ends ");
+
+	}
+	
+@Test(description="Verify Article Header Name After Selecting The Budget")
+	
+	public static void TC2_VerifyHeaderNameAfterSelectingBudget() {
+		
+		log.info("Verify Article Header Name After Selecting The Budget ... Starts ");
+		
+
+		// Click on the Menu Icon
+		UtilityClass.clickOnLocator(System.getProperty(CommonConstants.MenuXpath));
+		// Select Buget
+		UtilityClass.clickOnLocator(System.getProperty(CommonConstants.BudgetXpath));
+		// Get Header of Article
+		String HeaderName = UtilityClass.getTextOfLocator(System.getProperty(CommonConstants.ArticleheaderXapth));
+		System.out.println(HeaderName);
+		// verify Header should be same as the selcted option
+		Assert.assertTrue(HeaderName.equals("BUDGET"), "Article Header Name is Not Shown Proper");
+
+		log.info("Verify Article Header Name After Selecting The Budget... Ends ");
+
+	}
+
+@Test(description="Search Destination using search box ")
+
+public static void TC3_SearchDestinationUsingSearchBox() {
+	
+	log.info("Search Destination using search box ... Starts ");
+	
+
+	// Click on the search Icon
+	UtilityClass.clickOnLocator(System.getProperty(CommonConstants.ScearchIconXapth));
+	// Enter Test
+	UtilityClass.enterTextLocator(System.getProperty(CommonConstants.ScearchTextBoxXapth), "USA");
+	// Get Header of Article
+	String HeaderName = UtilityClass.getTextOfLocator(System.getProperty(CommonConstants.ArticleheaderXapth));
+	System.out.println(HeaderName);
+	// verify Header should be same as the selcted option
+	Assert.assertTrue(HeaderName.equals("CATEGORY"), "Article Header Name is Not Shown Proper");
+
+	log.info("Search Destination using search box.. Ends ");
+
+}
+
 
 }
